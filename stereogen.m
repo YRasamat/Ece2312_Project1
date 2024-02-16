e42efef96077b4c58ee72d3f75fa2d9f35b44254
YRasamat <67038710+YRasamat@users.noreply.github.com>
2024-02-16 11:40:40 -0500

    Update stereogen.m
    added semicolons

--- stereogen.m
+++ stereogen.m
@@ -1,5 +1,5 @@
 %Generate a stereo file from our given mono file
-outputFile = 'qbf-stereosoundfile-0ms.wav'
-data = audioread('quick_brown_fox.wav')
+outputFile = 'qbf-stereosoundfile-0ms.wav';
+data = audioread('quick_brown_fox.wav');
 data(:,2) = data(:,1);
 audiowrite(outputFile, data, fs)
