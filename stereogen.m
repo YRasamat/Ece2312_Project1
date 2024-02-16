56adddeec493c59ff5f4865b94bcc5dd308cb218
YRasamat <67038710+YRasamat@users.noreply.github.com>
2024-02-16 13:10:24 -0500

    Delete stereogen.m

--- stereogen.m
+++
@@ -1,5 +0,0 @@
-%Generate a stereo file from our given mono file
-outputFile = 'qbf-stereosoundfile-0ms.wav';
-data = audioread('quick_brown_fox.wav');
-data(:,2) = data(:,1);
-audiowrite(outputFile, data, fs)
